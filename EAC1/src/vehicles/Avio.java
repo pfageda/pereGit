13b9b5108ef81bca5ea65394cb930992f43eb2a6
perefajeda <perefajeda@gmail.com>
2022-10-06 20:14:26 +0200

    1ra pujada

---
+++ EAC1/src/vehicles/Avio.java
@@ -0,0 +1,23 @@
+package vehicles;
+
+public class Avio {
+        String model;
+        int passatgers;
+
+        public void setModel (String model){
+                this.model=model;
+        }
+
+        public String getModel (){
+                return model;
+        }
+
+        public void setPassatgers (int passatgers){
+                this.passatgers=passatgers;
+        }
+
+        public int getPassatgers (){
+                return passatgers;
+        }
+
+}
